083269cc21aa1321f929661216ea6e7545514740
Rima Dagher <rima.t.dagher@gmail.com>
2025-06-12 12:30:26 -0400

    Refactor movie fetch inside services/moviApi
    Fixed navbar style to stay fix on scrolling

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -2,7 +2,12 @@ import React from "react";
 
 const Favorites = () => {
   // List of saved MOvies
-  return <div>Favorites</div>;
+  return (
+    // Added main with Top padding so it doesn't hide behind nav
+    <main className="pt-24 p-6 space-y-12">
+      <h2>Favorites</h2>
+    </main>
+  );
 };
 
 export default Favorites;
--- src/pages/SearchMovie.js
+++ src/pages/SearchMovie.js
@@ -3,7 +3,12 @@ import React from "react";
 const SearchMovie = () => {
   console.log("In search Movie");
   // Search bar
-  return <div>SearchMovie</div>;
+  return (
+    // Added main with Top padding so it doesn't hide behind nav
+    <main className="pt-24 p-6 space-y-12">
+      <h2>Search</h2>
+    </main>
+  );
   // Grid of MovieCard (search results)
 };
 
