04a169b663ceb76d598a641df795af239c0b23e4
Ritu Arora <jsr.ritu.jsr@gmail.com>
2025-06-12 13:21:06 -0400

    made movieSection an independent component

---
+++ src/components/MovieSection.js
@@ -0,0 +1,23 @@
+import MovieCard from './MovieCard'
+
+const MovieSection = ({title, movies}) => {
+  return (
+    <div>
+      <section>
+            <h2 className="text-xl font-semibold mb-4 text-accent">
+              {title}
+            </h2>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+              {movies
+                .filter((movie) => movie.poster_path)
+                .slice(0, 8)
+                .map((movie) => (
+                  <MovieCard key={movie.id} movie={movie} />
+                ))}
+            </div>
+          </section>
+    </div>
+  )
+}
+
+export default MovieSection
