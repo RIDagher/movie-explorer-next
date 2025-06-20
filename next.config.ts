5aa4132bc9bae8bb32be50a3be66bdbf9aa6dbc5
Rima Dagher <rima.t.dagher@gmail.com>
2025-06-19 22:21:28 -0400

    Cleaned up Vercel config for proper Next.js 15 standalone deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "export", // Outputs a Single-Page Application (SPA) ###By removing this line This lets  dynamic routes (like [id]/page.js) fetch data at runtime based on what the user clicks.
-  distDir: "build", // Changes the build output directory to `build`
+  output: "standalone", // Outputs a Single-Page Application (SPA) ###By removing this line This lets  dynamic routes (like [id]/page.js) fetch data at runtime based on what the user clicks.
+  // distDir: "build", // Changes the build output directory to `build`
 };
 
 export default nextConfig;
